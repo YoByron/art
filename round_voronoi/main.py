946b98f93ab50d8d6f3da0740811edbfc3722502
bastihz <bastihz.dev@posteo.de>
2020-10-29 01:42:05 +0100

    Get rounded, closed polygons working

--- round_voronoi/main.py
+++ round_voronoi/main.py
@@ -1,7 +1,7 @@
 import numpy
 from typing import List, Tuple
-import scipy.spatial
-
+from geovoronoi import voronoi_regions_from_coords
+import shapely.geometry
 
 import pygame
 
@@ -9,50 +9,61 @@ import pygame
 WINDOW_SIZE = (1200, 800)
 WINDOW_DIAGONAL = numpy.hypot(*WINDOW_SIZE)
 WINDOW_RECT = pygame.Rect((0, 0), WINDOW_SIZE)
+WINDOW_POLYGON = shapely.geometry.Polygon((
+    WINDOW_RECT.topleft,
+    WINDOW_RECT.topright,
+    WINDOW_RECT.bottomright,
+    WINDOW_RECT.bottomleft
+))
 FPS = 60
 BACKGROUND_COLOR = pygame.Color(0, 32, 0)
-CIRCLE_COLOR = pygame.Color(0, 96, 255)
+CIRCLE_COLOR = pygame.Color(128, 96, 0)
 CIRCLE_RADIUS = 5
-POLYGON_COLOR = pygame.Color(255, 128, 0)
+LINE_WIDTH = 0
+POLYGON_COLOR = pygame.Color(32, 128, 32)
+CUT_RATIO = 0.25
+CUT_ITERATIONS = 4
 
 
-def generate_voronoi_polygons(points: List[Tuple[int, int]]) -> List[Tuple[float, float]]:
-    # see voronoi_plot_2d() in
-    # https://github.com/scipy/scipy/blob/master/scipy/spatial/_plotutils.py
-    # for how to plot the voronoi information
-    #
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.Voronoi.html
-
-    # scipy.spatial.Voronoi() does not work with less than 3 points.
+def generate_voronoi_polygons(points: List[Tuple[int, int]]) -> List[List[pygame.math.Vector2]]:
+    # Does not work with less than 3 points.
     if len(points) < 3:
         return []
-    vor = scipy.spatial.Voronoi(numpy.array(points))
+    poly_shapes = voronoi_regions_from_coords(
+        numpy.asarray(points),
+        WINDOW_POLYGON
+    )[0]
+    polygons = [poly.exterior.coords[:-1] for poly in poly_shapes]
+    for i, polygon in enumerate(polygons):
+        polygons[i] = [pygame.Vector2(p) for p in polygon]
+    return polygons
 
-    # FIXME: This is just copied from https://github.com/scipy/scipy/blob/master/scipy/spatial/_plotutils.py
-    #  Make it return the polygons surrounding the regions instead.
-    #  Test by filling the polygons in different colors.
 
-    center = vor.points.mean(axis=0)
-    segments = []
-    vor.ridge_vertices = numpy.asarray(vor.ridge_vertices)
-    for pointidx, simplex in zip(vor.ridge_points, vor.ridge_vertices):
-        if numpy.all(simplex >= 0):
-            segments.append(vor.vertices[simplex])
-        else:
-            i = simplex[simplex >= 0][0]  # finite end Voronoi vertex
-            near_point = vor.vertices[i]
+def cut(
+    points: List[pygame.math.Vector2],
+    ratio: float,
+    iterations: int = 1
+) -> List[pygame.math.Vector2]:
+    """ Chaikin's corner cutting: https://sighack.com/post/chaikin-curves
+    ratio: Ratio (between 0 and 1) of the edge length which
+        determines how close to the corner the cut should be made.
+    iterations: Number of iterations of the cutting algorithm.
+    """
+    # Avoid cutting over the line midpoint:
+    if ratio > 0.5:
+        ratio = 1 - ratio
 
-            t = vor.points[pointidx[1]] - vor.points[pointidx[0]]  # tangent
-            t /= numpy.linalg.norm(t)
-            n = numpy.array([-t[1], t[0]])  # normal
+    for _ in range(iterations):
+        new_points = []
+        n = len(points)
+        for i in range(n):
+            a = points[i]
+            b = points[(i + 1) % len(points)]
+            new_points.append(a.lerp(b, ratio))
+            new_points.append(b.lerp(a, ratio))
+        points = new_points
 
-            midpoint = vor.points[pointidx].mean(axis=0)
-            direction = numpy.sign(numpy.dot(midpoint - center, n)) * n
-            far_point = near_point + direction * WINDOW_DIAGONAL
-
-            segments.append((near_point, far_point))
-
-    return segments
+    return points
 
 
 def run() -> None:
@@ -61,12 +72,7 @@ def run() -> None:
     pygame.display.set_caption("art: round voronoi")
     clock = pygame.time.Clock()
     points = []
-
-    # DEBUG
-    debug_points = [(300, 200), (800, 100), (800, 700), (200, 600), (600, 400)]
-    for p in debug_points:
-        points.append(p)
-    polygons = generate_voronoi_polygons(points)
+    polygons = []
 
     running = True
     while running:
@@ -82,8 +88,12 @@ def run() -> None:
                 if event.button == 1:
                     points.append(event.pos)
                     polygons = generate_voronoi_polygons(points)
+                    polygons = [cut(polygon, CUT_RATIO, CUT_ITERATIONS) for polygon in polygons]
 
         window.fill(BACKGROUND_COLOR)
+
+        for polygon in polygons:
+            pygame.draw.polygon(window, POLYGON_COLOR, polygon, LINE_WIDTH)
 
         for point in points:
             pygame.draw.circle(
@@ -92,16 +102,6 @@ def run() -> None:
                 point,
                 CIRCLE_RADIUS
             )
-
-        for polygon in polygons:
-            pygame.draw.line(window, POLYGON_COLOR, polygon[0], polygon[1])
-        # NOTE: The polygons at the screen edges will be open.
-        #     pygame.draw.aalines(
-        #         window,
-        #         POLYGON_COLOR,
-        #         True,
-        #         polygon
-        #     )
 
         pygame.display.flip()
 
