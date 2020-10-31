60610e6444ae9ec6098c3821e5917bf4399c2cb5
bastihz <bastihz.dev@posteo.de>
2020-10-31 23:13:24 +0100

    Make the points move, spawn many points on right click, and start trying to improve the performance

--- helpers/voronoi.py
+++ helpers/voronoi.py
@@ -6,17 +6,19 @@ import shapely.geometry
 import pygame
 
 
-# TODO: Make a version that returns open polygons for points at the outer edge.
-# TODO: Use scipy to get the voronoi and then calculate the poygons yourself.
-#  See if that is more performant.
-
-
 def rect_to_shapely_polygon(rect: pygame.Rect) -> shapely.geometry.Polygon:
+    # Add a bit distance so that points are guaranteed to be removed before
+    # exiting the rect.
+    d = 1
+    left = rect.left - d
+    top = rect.top - d
+    right = rect.right + d
+    bottom = rect.bottom + d
     return shapely.geometry.Polygon((
-        rect.topleft,
-        rect.topright,
-        rect.bottomright,
-        rect.bottomleft
+        (left, top),
+        (right, top),
+        (right, bottom),
+        (left, bottom)
     ))
 
 
@@ -40,3 +42,68 @@ def generate_voronoi_polygons(
         polygons[i] = [pygame.Vector2(p) for p in polygon]
 
     return polygons
+
+
+import scipy.spatial
+
+
+# def voronoi_new(points: List[Tuple[int, int]]) -> List[List[pygame.math.Vector2]]:
+#     # # Does not work with less than 3 points.
+#     if len(points) < 3:
+#         return []
+#
+#     vor = scipy.spatial.Voronoi(numpy.array(points))
+#
+
+    # First calculate all lines where one end is at infinity (-1).
+    # Then go through the regions and build one polygon for each region.
+    # Then maybe sort the vertices in the polygons if they are not sorted?
+    # Close the outer polygons? Repair polygons at the four corners of the window?
+
+#     polygons = []
+#     for region in vor.regions:
+#         if region and -1 not in region:
+#             polygons.append([vor.vertices[i] for i in region])
+#
+#     return polygons
+
+
+# My first try at a voronoi function. Might become useful later.
+
+# def generate_voronoi_polygons(points: List[Tuple[int, int]]) -> List[Tuple[float, float]]:
+#     # see voronoi_plot_2d() in
+#     # https://github.com/scipy/scipy/blob/master/scipy/spatial/_plotutils.py
+#     # for how to plot the voronoi information
+#     #
+#     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.Voronoi.html
+#
+#     # scipy.spatial.Voronoi() does not work with less than 3 points.
+#     if len(points) < 3:
+#         return []
+#     vor = scipy.spatial.Voronoi(numpy.array(points))
+#
+#     # FIXME: This is just copied from https://github.com/scipy/scipy/blob/master/scipy/spatial/_plotutils.py
+#     #  Make it return the polygons surrounding the regions instead.
+#     #  Test by filling the polygons in different colors.
+#
+#     center = vor.points.mean(axis=0)
+#     segments = []
+#     vor.ridge_vertices = numpy.asarray(vor.ridge_vertices)
+#     for pointidx, simplex in zip(vor.ridge_points, vor.ridge_vertices):
+#         if numpy.all(simplex >= 0):
+#             segments.append(vor.vertices[simplex])
+#         else:
+#             i = simplex[simplex >= 0][0]  # finite end Voronoi vertex
+#             near_point = vor.vertices[i]
+#
+#             t = vor.points[pointidx[1]] - vor.points[pointidx[0]]  # tangent
+#             t /= numpy.linalg.norm(t)
+#             n = numpy.array([-t[1], t[0]])  # normal
+#
+#             midpoint = vor.points[pointidx].mean(axis=0)
+#             direction = numpy.sign(numpy.dot(midpoint - center, n)) * n
+#             far_point = near_point + direction * WINDOW_DIAGONAL
+#
+#             segments.append((near_point, far_point))
+#
+#     return segments
--- round_voronoi/main.py
+++ round_voronoi/main.py
@@ -1,30 +1,42 @@
-# TODO: Apply some noise to the cut_ratio and draw the polygons with alpha
-#  to give them a fuzzy edge.
-
 import os
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
-
 import sys
-sys.path.append(os.getcwd())
+from typing import Tuple
+import math
+import random
 
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
+import pygame.freetype
 
+sys.path.append(os.getcwd())
 from helpers import corner_cutter
 from helpers import voronoi
 
 
+SHOW_DEBUG_INFO = True
 WINDOW_SIZE = (1200, 800)
-WINDOW_POLYGON = voronoi.rect_to_shapely_polygon(pygame.Rect((0, 0), WINDOW_SIZE))
+WINDOW_RECT = pygame.Rect((0, 0), WINDOW_SIZE)
+WINDOW_POLYGON = voronoi.rect_to_shapely_polygon(WINDOW_RECT)
 FPS = 60
-BACKGROUND_COLOR = pygame.Color(32, 32, 32)
-CIRCLE_COLOR = pygame.Color(255, 128, 0)
+BACKGROUND_COLOR = pygame.Color(0, 255, 255)
+CIRCLE_COLOR = pygame.Color(255, 0, 0)
 CIRCLE_RADIUS = 5
 LINE_WIDTH = 3
 DRAW_OUTLINES = True
-POLYGON_FILL_COLOR = pygame.Color(32, 128, 32)
+POLYGON_FILL_COLOR = pygame.Color(32, 32, 32)
 POLYGON_LINE_COLOR = pygame.Color(0, 255, 255)
 CUT_RATIO = 0.15
-CUT_ITERATIONS = 4
+CUT_ITERATIONS = 3
+SPEED_MODIFIER = 50
+SPREAD = 50
+NUM_CIRCLES_PER_RIGHT_CLICK = 50
+
+
+def get_move_vector(point: Tuple[int, int]) -> Tuple[float, float]:
+    dx = point[0] - WINDOW_RECT.centerx
+    dy = point[1] - WINDOW_RECT.centery
+    length = math.hypot(dx, dy) / SPEED_MODIFIER
+    return dx / length, dy / length
 
 
 def run() -> None:
@@ -32,12 +44,20 @@ def run() -> None:
     window = pygame.display.set_mode(WINDOW_SIZE)
     pygame.display.set_caption("art: round voronoi")
     clock = pygame.time.Clock()
+    font = pygame.freetype.SysFont("inconsolate, consolas, monospace", 16)
+    font.fgcolor = pygame.Color(255, 255, 255)
+    font.bgcolor = POLYGON_FILL_COLOR
     points = []
     polygons = []
+    move_vectors = []
+
+    # DEBUG:
+    points = [(580, 200), (500, 370), (730, 310), (600, 290)]
+    move_vectors = [get_move_vector(p) for p in points]
 
     running = True
     while running:
-        clock.tick(FPS)
+        dt = clock.tick(FPS) / 1000
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -48,14 +68,33 @@ def run() -> None:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     points.append(event.pos)
-                    polygons = voronoi.generate_voronoi_polygons(
-                        points,
-                        WINDOW_POLYGON
-                    )
-                    polygons = [
-                        corner_cutter.cut(polygon, CUT_RATIO, CUT_ITERATIONS)
-                        for polygon in polygons
-                    ]
+                    move_vectors.append(get_move_vector(event.pos))
+                elif event.button == 3:
+                    for _ in range(NUM_CIRCLES_PER_RIGHT_CLICK):
+                        x, y = event.pos
+                        x += random.randint(-SPREAD, SPREAD)
+                        y += random.randint(-SPREAD, SPREAD)
+                        points.append((x, y))
+                        move_vectors.append(get_move_vector((x, y)))
+
+        keep = []
+        for i, (p, mv) in enumerate(zip(points, move_vectors)):
+            x = p[0] + (mv[0] * dt)
+            y = p[1] + (mv[1] * dt)
+            points[i] = (x, y)
+            if WINDOW_RECT.collidepoint(x, y):
+                keep.append(i)
+        points = [points[i] for i in keep]
+        move_vectors = [move_vectors[i] for i in keep]
+
+        polygons = voronoi.generate_voronoi_polygons(
+            points,
+            WINDOW_POLYGON
+        )
+        polygons = [
+            corner_cutter.cut(polygon, CUT_RATIO, CUT_ITERATIONS)
+            for polygon in polygons
+        ]
 
         window.fill(BACKGROUND_COLOR)
         for polygon in polygons:
@@ -64,6 +103,25 @@ def run() -> None:
                 pygame.draw.polygon(window, POLYGON_LINE_COLOR, polygon, LINE_WIDTH)
         for point in points:
             pygame.draw.circle(window, CIRCLE_COLOR, point, CIRCLE_RADIUS)
+        pygame.draw.rect(window, POLYGON_LINE_COLOR, WINDOW_RECT, LINE_WIDTH)
+
+        if SHOW_DEBUG_INFO:
+            font.render_to(
+                window,
+                (5, 5),
+                f"fps: {clock.get_fps():.0f}"
+            )
+            font.render_to(
+                window,
+                (5, 25),
+                f"{len(polygons)=}"
+            )
+            font.render_to(
+                window,
+                (5, 45),
+                f"{sum(len(p) for p in polygons)=}"
+            )
+
         pygame.display.flip()
 
 
