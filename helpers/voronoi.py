7efe15145189919721036e2f738cbc3340878fff
bastihz <bastihz.dev@posteo.de>
2020-11-01 16:18:05 +0100

    Use scipy again for voronoi polygon generation

--- helpers/voronoi.py
+++ helpers/voronoi.py
@@ -1,71 +1,18 @@
 from typing import List, Tuple
-import numpy
-from geovoronoi import voronoi_regions_from_coords
-import shapely.geometry
 
+import scipy.spatial
+import numpy
 import pygame
 
 
-def rect_to_shapely_polygon(rect: pygame.Rect) -> shapely.geometry.Polygon:
-    # Add a bit distance so that points are guaranteed to be removed before
-    # exiting the rect.
-    d = 1
-    left = rect.left - d
-    top = rect.top - d
-    right = rect.right + d
-    bottom = rect.bottom + d
-    return shapely.geometry.Polygon((
-        (left, top),
-        (right, top),
-        (right, bottom),
-        (left, bottom)
-    ))
-
-
-def generate_voronoi_polygons(
-        points: List[Tuple[int, int]],
-        outer_polygon: shapely.geometry.Polygon
-) -> List[List[pygame.math.Vector2]]:
-    """Convert a list of points into a list of polygons. Points at
-    the outer edge of the point cloud result in closed polygons.
-    """
-    # Does not work with less than 3 points.
-    if len(points) < 3:
-        return []
-
-    poly_shapes = voronoi_regions_from_coords(
-        numpy.asarray(points),
-        outer_polygon
-    )[0]
-    polygons = [ps.exterior.coords[:-1] for ps in poly_shapes]
-    for i, polygon in enumerate(polygons):
-        polygons[i] = [pygame.Vector2(p) for p in polygon]
-
+def get_voronoi_polygons(points: List[Tuple[int, int]]) -> List[List[pygame.math.Vector2]]:
+    vor = scipy.spatial.Voronoi(numpy.asarray(points))
+    vertices = [pygame.Vector2(*v) for v in vor.vertices]
+    polygons = []
+    for region in vor.regions:
+        if region and -1 not in region:
+            polygons.append([vertices[i] for i in region])
     return polygons
-
-
-import scipy.spatial
-
-
-# def voronoi_new(points: List[Tuple[int, int]]) -> List[List[pygame.math.Vector2]]:
-#     # # Does not work with less than 3 points.
-#     if len(points) < 3:
-#         return []
-#
-#     vor = scipy.spatial.Voronoi(numpy.array(points))
-#
-
-    # First calculate all lines where one end is at infinity (-1).
-    # Then go through the regions and build one polygon for each region.
-    # Then maybe sort the vertices in the polygons if they are not sorted?
-    # Close the outer polygons? Repair polygons at the four corners of the window?
-
-#     polygons = []
-#     for region in vor.regions:
-#         if region and -1 not in region:
-#             polygons.append([vor.vertices[i] for i in region])
-#
-#     return polygons
 
 
 # My first try at a voronoi function. Might become useful later.
@@ -82,9 +29,6 @@ import scipy.spatial
 #         return []
 #     vor = scipy.spatial.Voronoi(numpy.array(points))
 #
-#     # FIXME: This is just copied from https://github.com/scipy/scipy/blob/master/scipy/spatial/_plotutils.py
-#     #  Make it return the polygons surrounding the regions instead.
-#     #  Test by filling the polygons in different colors.
 #
 #     center = vor.points.mean(axis=0)
 #     segments = []
--- round_voronoi/main.py
+++ round_voronoi/main.py
@@ -13,19 +13,17 @@ from helpers import corner_cutter
 from helpers import voronoi
 
 
-SHOW_DEBUG_INFO = True
 WINDOW_SIZE = (1200, 800)
 WINDOW_RECT = pygame.Rect((0, 0), WINDOW_SIZE)
-WINDOW_POLYGON = voronoi.rect_to_shapely_polygon(WINDOW_RECT)
 FPS = 60
 BACKGROUND_COLOR = pygame.Color(0, 255, 255)
-CIRCLE_COLOR = pygame.Color(0, 255, 255)
+CIRCLE_COLOR = pygame.Color(255, 128, 0)  # good visibility for debugging
 CIRCLE_RADIUS = 5
 LINE_WIDTH = 3
 BORDER_WIDTH = 10
 DRAW_OUTLINES = True
 POLYGON_FILL_COLOR = pygame.Color(32, 32, 32)
-POLYGON_LINE_COLOR = pygame.Color(0, 255, 255)
+POLYGON_LINE_COLOR = BACKGROUND_COLOR
 CUT_RATIO = 0.15
 CUT_ITERATIONS = 3
 SPEED_MODIFIER = 50
@@ -37,10 +35,14 @@ def get_move_vector(point: Tuple[int, int]) -> Tuple[float, float]:
     dx = point[0] - WINDOW_RECT.centerx
     dy = point[1] - WINDOW_RECT.centery
     length = math.hypot(dx, dy) / SPEED_MODIFIER
-    return dx / length, dy / length
+    if length > 0:
+        return dx / length, dy / length
+    else:
+        return SPEED_MODIFIER, SPEED_MODIFIER
 
 
 def run() -> None:
+    show_debug_info = True
     pygame.init()
     window = pygame.display.set_mode(WINDOW_SIZE)
     pygame.display.set_caption("art: round voronoi")
@@ -48,11 +50,12 @@ def run() -> None:
     font = pygame.freetype.SysFont("inconsolate, consolas, monospace", 16)
     font.fgcolor = pygame.Color(255, 255, 255)
     font.bgcolor = POLYGON_FILL_COLOR
-    points = []
-    move_vectors = []
 
-    # DEBUG:
-    points = [(580, 200), (500, 370), (730, 310), (600, 290)]
+    left = WINDOW_RECT.left - WINDOW_RECT.width
+    right = WINDOW_RECT.width * 2
+    top = WINDOW_RECT.top - WINDOW_RECT.height
+    bottom = WINDOW_RECT.bottom * 2
+    points = [(left, top), (right, top), (right, bottom), (left, bottom)]
     move_vectors = [get_move_vector(p) for p in points]
 
     running = True
@@ -65,6 +68,8 @@ def run() -> None:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                elif event.key == pygame.K_F1:
+                    show_debug_info = not show_debug_info
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     points.append(event.pos)
@@ -83,23 +88,20 @@ def run() -> None:
             y = p[1] + (mv[1] * dt)
             if WINDOW_RECT.collidepoint(x, y):
                 points[i] = (x, y)
-            else:
+            elif i > 3:
+                # > 3 to not delete the 4 additional corner points
                 del points[i]
                 del move_vectors[i]
 
-        polygons = voronoi.generate_voronoi_polygons(
-            points,
-            WINDOW_POLYGON
-        )
+        polygons = voronoi.get_voronoi_polygons(points)
         polygons = [
             corner_cutter.cut(polygon, CUT_RATIO, CUT_ITERATIONS)
             for polygon in polygons
         ]
 
-        if len(points) > 2:
+        if len(polygons) > 0:
             window.fill(BACKGROUND_COLOR)
         else:
-            # Voronoi does not work with less than three points.
             window.fill(POLYGON_FILL_COLOR)
         for polygon in polygons:
             pygame.draw.polygon(window, POLYGON_FILL_COLOR, polygon)
@@ -109,7 +111,7 @@ def run() -> None:
             pygame.draw.circle(window, CIRCLE_COLOR, point, CIRCLE_RADIUS)
         pygame.draw.rect(window, POLYGON_LINE_COLOR, WINDOW_RECT, BORDER_WIDTH)
 
-        if SHOW_DEBUG_INFO:
+        if show_debug_info:
             font.render_to(
                 window,
                 (5, 5),
@@ -118,11 +120,16 @@ def run() -> None:
             font.render_to(
                 window,
                 (5, 25),
-                f"{len(polygons)=}"
+                f"{len(points)=}"
             )
             font.render_to(
                 window,
                 (5, 45),
+                f"{len(polygons)=}"
+            )
+            font.render_to(
+                window,
+                (5, 65),
                 f"{sum(len(p) for p in polygons)=}"
             )
 
