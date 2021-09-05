d9a943689d33e7a9bee12c64197e714157ee38db
bastihz <bastihz.dev@posteo.de>
2021-09-05 20:41:19 +0200

    Make topple() do just one iteration

--- java/sandpiles/src/main/java/SandPiles.java
+++ java/sandpiles/src/main/java/SandPiles.java
@@ -5,6 +5,8 @@
 // Using only a single array is much faster but then the animation is no longer symmetric.
 // TODO: Use symmetry to iterate over only a quarter of the pile. Then be clever with
 //  the square coordinates so you don't have to copy the quarter to the other tree parts.
+// But that only works if I drop sand into that quarter. Someday I want to make it interactive,
+// so that the user can drop some sand by clicking the mouse. But then speed won't be so important.
 
 import processing.core.PApplet;
 
@@ -13,7 +15,7 @@ public class SandPiles extends PApplet {
     private static int defaultWidth = 800;
     private static int defaultHeight = 800;
 
-    private static final boolean ANIMATE = true;
+    private static final boolean ANIMATE = false;
     private static final int N_SAND = 250_000;
     private final int[] colors = new int[] {
             color(10, 63, 255),
@@ -21,8 +23,8 @@ public class SandPiles extends PApplet {
             color(255, 222, 0),
             color(123, 0, 0)
     };
-    private int widthHalf;
-    private int heightHalf;
+    private int pileWidth;
+    private int pileHeight;
     private int[] pile;
     private int[] nextPile;
 
@@ -45,62 +47,68 @@ public class SandPiles extends PApplet {
             noLoop();
         }
         noStroke();
-        widthHalf = width / 2;
-        heightHalf = height / 2;
-        pile = new int[widthHalf * heightHalf];
+        pileWidth = width / 2;
+        pileHeight = height / 2;
+        pile = new int[pileWidth * pileHeight];
         nextPile = pile.clone();
-        pile[pile.length / 2 - widthHalf / 2] = N_SAND;
+        pile[pile.length / 2 - pileWidth / 2] = N_SAND;
     }
 
     @Override
     public void draw() {
-        topple();
 
-        for (int i = 0; i < pile.length; i++) {
-            final int n = pile[i];
-            fill(n < 4 ? colors[n] : color(255));
-            final int x = i % widthHalf * 2;
-            final int y = i / heightHalf * 2;
-            square(x, y, 2);
+        if (ANIMATE) {
+            topple();
+        } else {
+            final long startTime = System.nanoTime();
+            boolean notFinished = true;
+            while (notFinished) {
+                notFinished = topple();
+            }
+            final long endTime = System.nanoTime();
+            System.out.println((endTime - startTime) / 1_000_000_000.0);
+        }
+
+        for (int x = 0; x < pileWidth; x++) {
+            for (int y = 0; y < pileHeight; y++) {
+                final int n = pile[x + y * pileWidth];
+                fill(n < 4 ? colors[n] : color(255));
+                square(x + x, y + y, 2);
+            }
         }
     }
 
-    private void topple() {
-        // final long startTime = System.nanoTime();
-        boolean finished;
-        do {
-            finished = true;
-            for (int i = 0; i < pile.length; i++) {
-                final int n = pile[i];
-                if (n >= 4) {
-                    finished = false;
-                    final int nToDistribute = n / 4;
-                    nextPile[i] = n % 4;
+    private boolean topple() {
+        boolean notFinished = false;
+        for (int i = 0; i < pile.length; i++) {
+            final int n = pile[i];
+            if (n >= 4) {
+                notFinished = true;
+                final int nToDistribute = n / 4;
+                nextPile[i] = n % 4;
 
-                    // left neighbor
-                    if (i > 0) {
-                        nextPile[i - 1] += nToDistribute;
-                    }
+                // left neighbor
+                if (i > 0) {
+                    nextPile[i - 1] += nToDistribute;
+                }
 
-                    // right neighbor
-                    if (i < pile.length - 1) {
-                        nextPile[i + 1] += nToDistribute;
-                    }
+                // right neighbor
+                if (i < pile.length - 1) {
+                    nextPile[i + 1] += nToDistribute;
+                }
 
-                    // top neighbor
-                    if (i >= widthHalf) {
-                        nextPile[i - widthHalf] += nToDistribute;
-                    }
+                // top neighbor
+                if (i >= pileWidth) {
+                    nextPile[i - pileWidth] += nToDistribute;
+                }
 
-                    // bottom neighbor
-                    if (i < pile.length - widthHalf) {
-                        nextPile[i + widthHalf] += nToDistribute;
-                    }
+                // bottom neighbor
+                if (i < pile.length - pileWidth) {
+                    nextPile[i + pileWidth] += nToDistribute;
                 }
             }
-            System.arraycopy(nextPile, 0, pile, 0, pile.length);
-        } while (!finished && !ANIMATE);
-        // final long endTime = System.nanoTime();
-        // System.out.println((endTime - startTime) / 1_000_000_000.0);
+        }
+        System.arraycopy(nextPile, 0, pile, 0, pile.length);
+        return notFinished;
     }
 }
