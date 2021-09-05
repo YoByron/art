ea2fe48bc8951ea6b9614b725c71750962c74c2e
bastihz <bastihz.dev@posteo.de>
2021-09-05 15:38:59 +0200

    Animate the pile toppling

--- java/sandpiles/src/main/java/SandPiles.java
+++ java/sandpiles/src/main/java/SandPiles.java
@@ -1,6 +1,11 @@
 // https://en.wikipedia.org/wiki/Abelian_sandpile_model
 // https://www.youtube.com/watch?v=1MtEUErz7Gg
 
+// This approach uses two arrays to store the current and the next state of the pile.
+// Using only a single array is much faster but then the animation is no longer symmetric.
+// TODO: Use symmetry to iterate over only a quarter of the pile. Then be clever with
+//  the square coordinates so you don't have to copy the quarter to the other tree parts.
+
 import processing.core.PApplet;
 
 public class SandPiles extends PApplet {
@@ -8,6 +13,7 @@ public class SandPiles extends PApplet {
     private static int defaultWidth = 800;
     private static int defaultHeight = 800;
 
+    private static final boolean ANIMATE = true;
     private static final int N_SAND = 250_000;
     private final int[] colors = new int[] {
             color(10, 63, 255),
@@ -15,6 +21,10 @@ public class SandPiles extends PApplet {
             color(255, 222, 0),
             color(123, 0, 0)
     };
+    private int widthHalf;
+    private int heightHalf;
+    private int[] pile;
+    private int[] nextPile;
 
     public static void main(final String[] args) {
         if (args.length == 2) {
@@ -31,57 +41,66 @@ public class SandPiles extends PApplet {
 
     @Override
     public void setup() {
-        noLoop();
+        if (!ANIMATE) {
+            noLoop();
+        }
         noStroke();
+        widthHalf = width / 2;
+        heightHalf = height / 2;
+        pile = new int[widthHalf * heightHalf];
+        nextPile = pile.clone();
+        pile[pile.length / 2 - widthHalf / 2] = N_SAND;
     }
 
     @Override
     public void draw() {
-        final int widthHalf = width / 2;
-        final int heightHalf = height / 2;
-        final int[] pile = new int[widthHalf * heightHalf];
-        pile[pile.length / 2 - widthHalf / 2] = N_SAND;
+        topple();
 
-        boolean finished = false;
-        while (!finished) {
+        for (int i = 0; i < pile.length; i++) {
+            final int n = pile[i];
+            fill(n < 4 ? colors[n] : color(255));
+            final int x = i % widthHalf * 2;
+            final int y = i / heightHalf * 2;
+            square(x, y, 2);
+        }
+    }
+
+    private void topple() {
+        // final long startTime = System.nanoTime();
+        boolean finished;
+        do {
             finished = true;
             for (int i = 0; i < pile.length; i++) {
                 final int n = pile[i];
                 if (n >= 4) {
                     finished = false;
-                    final int nForNeighbor = n / 4;
-                    pile[i] = n % 4;
+                    final int nToDistribute = n / 4;
+                    nextPile[i] = n % 4;
 
                     // left neighbor
                     if (i > 0) {
-                        pile[i - 1] += nForNeighbor;
+                        nextPile[i - 1] += nToDistribute;
                     }
 
                     // right neighbor
                     if (i < pile.length - 1) {
-                        pile[i + 1] += nForNeighbor;
+                        nextPile[i + 1] += nToDistribute;
                     }
 
                     // top neighbor
-                    final int topIndex = i - widthHalf;
-                    if (topIndex >= 0) {
-                        pile[topIndex] += nForNeighbor;
+                    if (i >= widthHalf) {
+                        nextPile[i - widthHalf] += nToDistribute;
                     }
 
                     // bottom neighbor
-                    final int bottomIndex = i + widthHalf;
-                    if (bottomIndex < pile.length - 1) {
-                        pile[bottomIndex] += nForNeighbor;
+                    if (i < pile.length - widthHalf) {
+                        nextPile[i + widthHalf] += nToDistribute;
                     }
                 }
             }
-        }
-
-        for (int i = 0; i < pile.length; i++) {
-            final int x = i % widthHalf;
-            final int y = i / heightHalf;
-            fill(colors[pile[i]]);
-            square(x + x, y + y, 2);
-        }
+            System.arraycopy(nextPile, 0, pile, 0, pile.length);
+        } while (!finished && !ANIMATE);
+        // final long endTime = System.nanoTime();
+        // System.out.println((endTime - startTime) / 1_000_000_000.0);
     }
 }
